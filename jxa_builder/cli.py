d1a5949110a768474a2fa31a51d3c577ab2872bf
seb0xff <contact.git@seb0xff.dev>
2024-12-23 13:29:18 +0100

    Add nodejs deps freezing

--- jxa_builder/cli.py
+++ jxa_builder/cli.py
@@ -1,15 +1,8 @@
-#!/usr/bin/env python3
-import os
-import shutil
-from typing import Literal, TypeVar, Generic, Iterable, Optional, Union, cast, get_type_hints, Protocol, runtime_checkable, List, Dict, Sequence, Tuple
-from types import SimpleNamespace
-from pprint import pprint
-from os import path as p
 from jxa_builder.utils.click_importer import click
-
 from jxa_builder.commands.build import build
 from jxa_builder.commands.install import install
 from jxa_builder.commands.uninstall import uninstall
+from jxa_builder.commands.freeze_nodejs_deps import freeze_nodejs_deps
 
 
 @click.group(
@@ -24,6 +17,7 @@ def cli():
 cli.add_command(build)
 cli.add_command(install)
 cli.add_command(uninstall)
+cli.add_command(freeze_nodejs_deps)
 
 if __name__ == "__main__":
   cli()
---
+++ jxa_builder/commands/freeze_nodejs_deps.py
@@ -0,0 +1,40 @@
+import os
+from os import path as p
+import shutil
+from jxa_builder.core.get_dependency_modules import get_dependency_modules
+from jxa_builder.core.get_project_config import get_project_config
+from jxa_builder.core.constants import DEPS_DIR, NODE_DIR
+from jxa_builder.utils.click_importer import click
+from jxa_builder.utils.printit import log_print_error
+from jxa_builder.commands._shared_options import debug_option, project_dir_option
+
+
+@click.command(
+    help=
+    'Copy nodejs dependencies to the dependencies directory. It is useful when the target needs to be built on a machine without nodejs installed'
+)
+@project_dir_option
+@debug_option
+def freeze_nodejs_deps(project_dir: str):
+  jxa_config = get_project_config(project_dir)
+
+  dependency_modules = get_dependency_modules(project_dir)
+
+  if freeze_nodejs_deps:
+    for dep in dependency_modules:
+      try:
+        if NODE_DIR in dep.source and p.exists(dep.source):
+          node_modules = p.join(jxa_config.project_dir, NODE_DIR)
+          for root, dirs, _ in os.walk(node_modules):
+            for d in dirs:
+              if d == dep.name:
+                shutil.copytree(p.join(root, d),
+                                p.join(jxa_config.project_dir, DEPS_DIR,
+                                       dep.name),
+                                dirs_exist_ok=True)
+                break
+          if not p.exists(p.join(jxa_config.project_dir, DEPS_DIR)):
+            os.makedirs(p.join(jxa_config.project_dir, DEPS_DIR))
+      except Exception as e:
+        log_print_error(f'Cannot copy nodejs dependency: {dep}: {e}')
+        exit(1)
