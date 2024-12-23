d3bce381b77b46a3c51aca0b36dccfffded2c469
seb0xff <contact.git@seb0xff.dev>
2024-12-23 13:29:18 +0100

    Add uninstall command

--- jxa_builder/commands/uninstall.py
+++ jxa_builder/commands/uninstall.py
@@ -1,11 +1,20 @@
-from typing import Optional
+from os import getcwd, path as p
 from jxa_builder.utils.click_importer import click
 from jxa_builder.core.manage_outputs import manage_outputs
+from ._shared_options import debug_option
+from jxa_builder.core.constants import LOCATIONS_FILE
 
 
 @click.command()
-@click.option('--locations-file',
-              default='./build/locations.json',
-              help='Locations file')
-def uninstall(locations_file: Optional[str]):
-  click.echo(f"{locations_file}")
+@click.option('--path',
+              type=click.Path(exists=True),
+              default=getcwd(),
+              help='Path to the project directory or the locations.json file')
+@debug_option
+def uninstall(path: str):
+  """path: Path to the project directory or locations file"""
+  print(f"{path}")
+  if p.isfile(path):
+    manage_outputs('uninstall', path, False)
+  elif p.isdir(path):
+    manage_outputs('uninstall', p.join(path, LOCATIONS_FILE), False)
