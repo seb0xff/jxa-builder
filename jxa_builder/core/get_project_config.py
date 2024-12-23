b7a6d725cb7fae1f86796b54775be232728a28fa
seb0xff <contact.git@seb0xff.dev>
2024-12-23 13:29:18 +0100

    Add config model (no validation yet)

--- jxa_builder/core/get_project_config.py
+++ jxa_builder/core/get_project_config.py
@@ -1,16 +1,13 @@
-from typing import Optional, Literal, Dict, Callable
-from typing_extensions import Annotated
+from typing import Optional, Literal, Dict, Callable, List, Dict, Tuple
 import os
 from os import path as p
 import json
-from pydantic import (BaseModel, ConfigDict, ValidationError,
-                      StringConstraints, field_validator)
-from pydantic.alias_generators import to_camel, to_snake
 from jxa_builder.core.constants import PACKAGE_JSON_FILE, JXA_JSON_FILE
 from jxa_builder.utils.remove_empty_values import remove_empty_values
 from jxa_builder.utils.logger import logger
+from jxa_builder.utils.recase import recase
 from jxa_builder.utils.printit import terminate_with_error
-from jxa_builder.core.models import JxaProjectConfig, SEM_VER
+from jxa_builder.core.models import JxaProjectConfig, SEM_VER, LoadedPropInfo
 
 
 def get_json_obj(file_path: str) -> Dict[str, any]:
@@ -37,114 +34,113 @@ def get_json_obj(file_path: str) -> Dict[str, any]:
 ErrorsNumber = int
 OriginalName = str
 
+# def validate_dict_with_model(
+#     json_obj: Dict[str, any],
+#     *,
+#     error_msg_header: Optional[Callable[[ErrorsNumber], str]] = None,
+#     prop_alt_name: Optional[Callable[[OriginalName], str]] = None,
+# ):
+#   try:
+#     logger.debug(f'Validating: {json_obj}')
+#     JxaProjectConfig(**json_obj)
+#   except ValidationError as e:
+#     errors = e.errors()
+#     error_msg = ''
+#     if error_msg_header:
+#       error_msg += error_msg_header(len(errors))
+#     else:
+#       error_msg += f'\n{len(errors)} validation error(s)'
+#     for error in e.errors():
+#       prop = error['loc'][0]
+#       if prop_alt_name:
+#         prop = prop_alt_name(prop)
+#       error_msg += f'\n{prop}'
+#       if error['type'] == 'extra_forbidden':
+#         error_msg += f'''\n  Unrecognized property (input value: '{error["input"]}')'''
+#       elif error['type'] == 'string_pattern_mismatch' and error['ctx'][
+#           'pattern'] == SEM_VER:
+#         error_msg += f'''\n  Input should be a valid semantic version (input value: '{error["input"]}')'''
+#       elif error['type'] == 'value_error':
+#         error_msg += f'''\n  {error["msg"][error["msg"].find(', ')+2:]} (input value: '{error["input"]}')'''
+#       else:
+#         error_msg += f'''\n  {error["msg"]} (input value: '{error["input"]}')'''
 
-def validate_dict_with_model(
-    json_obj: Dict[str, any],
-    *,
-    error_msg_header: Optional[Callable[[ErrorsNumber], str]] = None,
-    prop_alt_name: Optional[Callable[[OriginalName], str]] = None,
-):
-  try:
-    logger.debug(f'Validating: {json_obj}')
-    JxaProjectConfig(**json_obj)
-  except ValidationError as e:
-    errors = e.errors()
-    error_msg = ''
-    if error_msg_header:
-      error_msg += error_msg_header(len(errors))
-    else:
-      error_msg += f'\n{len(errors)} validation error(s)'
-    for error in e.errors():
-      prop = error['loc'][0]
-      if prop_alt_name:
-        prop = prop_alt_name(prop)
-      error_msg += f'\n{prop}'
-      if error['type'] == 'extra_forbidden':
-        error_msg += f'''\n  Unrecognized property (input value: '{error["input"]}')'''
-      elif error['type'] == 'string_pattern_mismatch' and error['ctx'][
-          'pattern'] == SEM_VER:
-        error_msg += f'''\n  Input should be a valid semantic version (input value: '{error["input"]}')'''
-      elif error['type'] == 'value_error':
-        error_msg += f'''\n  {error["msg"][error["msg"].find(', ')+2:]} (input value: '{error["input"]}')'''
-      else:
-        error_msg += f'''\n  {error["msg"]} (input value: '{error["input"]}')'''
-
-    terminate_with_error(error_msg)
+#     terminate_with_error(error_msg)
 
 
-def cmd_args_override(cmd_overrides: Dict[str, any]) -> Dict[str, any]:
-  if cmd_overrides:
-    cmd_overrides = {to_camel(k): v for k, v in cmd_overrides.items()}
-    cmd_overrides = remove_empty_values(cmd_overrides)
-    to_kebab = lambda s: s.replace('_', '-')
-    validate_dict_with_model(
-        cmd_overrides,
-        error_msg_header=lambda n:
-        f'{n} validation error(s) in command line arguments',
-        prop_alt_name=lambda s: f'--{to_kebab(to_snake(s))}',
-    )
-  return cmd_overrides
+def validate_dict(dict: Dict[str, any]):
+  ...
 
 
 def get_project_config(
     project_dir: str,
-    override_func: Optional[Callable[..., Dict[str, any]]] = None
-) -> 'JxaProjectConfig':
+    cmd_overrides: Optional[Dict[str, any]] = None) -> 'JxaProjectConfig':
   logger.debug(f'Getting project config of: {project_dir}')
-  if not p.exists(project_dir):
-    terminate_with_error(f'"{project_dir}" does not exist')
-  if not p.isdir(project_dir):
-    terminate_with_error(f'"{project_dir}" is not a directory')
+
+  def to_snake_dict(d: Dict[str, any]) -> Dict[str, any]:
+    return {recase(k, 'snake'): v for k, v in d.items()}
+
+  properties: Dict[str, LoadedPropInfo] = {}
 
   package_json_file = p.join(project_dir, PACKAGE_JSON_FILE)
   jxa_json_file = p.join(project_dir, JXA_JSON_FILE)
-  project_config = JxaProjectConfig()
 
   # package.json
   package_json = get_json_obj(package_json_file)
-  name = package_json.get('name')
-  version = package_json.get('version')
-  main = package_json.get('main')
-  package_json = {'appName': name, 'version': version, 'main': main}
+  package_json = {
+      'app_name': package_json.get('name'),
+      'version': package_json.get('version'),
+      'main': package_json.get('main'),
+  }
   package_json = remove_empty_values(package_json)
   if package_json:
-    validate_dict_with_model(
-        package_json,
-        error_msg_header=lambda n:
-        f'{n} validation error(s) in "{package_json_file}"',
-    )
-    project_config = project_config.model_copy(update=package_json)
+    properties.update({
+        k:
+        LoadedPropInfo(v, package_json_file, recase(k, 'camel'))
+        for k, v in package_json.items()
+    })
 
   # package.json jxa property
   package_json = get_json_obj(package_json_file)
   package_json = remove_empty_values(package_json)
   package_json_jxa = package_json.get('jxa', {})
+  package_json_jxa = to_snake_dict(package_json_jxa)
   if not isinstance(package_json_jxa, dict):
     terminate_with_error(
         f'Invalid type in "{package_json_file}": The "jxa" must be an object.')
   if package_json_jxa:
-    validate_dict_with_model(
-        package_json_jxa,
-        error_msg_header=lambda n:
-        f'{n} validation error(s) in "{package_json_file}"',
-        prop_alt_name=lambda s: f'jxa.{s}',
-    )
-    project_config = project_config.model_copy(update=package_json_jxa)
+    properties.update({
+        k:
+        LoadedPropInfo(v, f'{package_json_file} "jxa" property',
+                       recase(k, 'camel'))
+        for k, v in package_json_jxa.items()
+    })
 
-    # jxa.json
-    jxa_json = get_json_obj(jxa_json_file)
-    jxa_json = remove_empty_values(jxa_json)
-    if jxa_json:
-      validate_dict_with_model(
-          jxa_json,
-          error_msg_header=lambda n:
-          f'{n} validation error(s) in "{jxa_json_file}"',
-      )
-      project_config = project_config.model_copy(update=jxa_json)
+  # jxa.json
+  jxa_json = get_json_obj(jxa_json_file)
+  jxa_json = remove_empty_values(jxa_json)
+  jxa_json = to_snake_dict(jxa_json)
+  if jxa_json:
+    properties.update({
+        k: LoadedPropInfo(v, jxa_json_file, recase(k, 'camel'))
+        for k, v in jxa_json.items()
+    })
 
-    # overrides
-    if override_func:
-      overrides = override_func()
-      if overrides:
-        project_config = project_config.model_copy(update=overrides)
-  return project_config
+  # overrides
+  if cmd_overrides:
+    cmd_overrides = remove_empty_values(cmd_overrides)
+    cmd_overrides = to_snake_dict(cmd_overrides)
+    properties.update({
+        k:
+        LoadedPropInfo(v, 'command line', f'--{recase(k, "kebab")}')
+        for k, v in cmd_overrides.items()
+    })
+
+  validate_dict(properties)
+
+  return JxaProjectConfig({k: v.value for k, v in properties.items()})
+
+
+if __name__ == '__main__':
+  config = get_project_config(
+      '/Users/sebastian/Documents/src/js/DoubleClickConsoleEdit')
--- jxa_builder/core/models.py
+++ jxa_builder/core/models.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from os import path as p
-from typing import Optional, Literal, Union
+from typing import Optional, Literal, Union, Callable
 from typing_extensions import Annotated
 from pydantic import (BaseModel, ConfigDict, StringConstraints, ValidationInfo,
                       Field, field_validator, computed_field)
@@ -23,65 +23,82 @@ class Module:
 
 SEM_VER = r'^(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$'
 
+# def _does_path_exist(path: Optional[str] = None,
+#                      info: Optional[ValidationInfo] = None) -> bool:
+#   if path:
+#     path = p.abspath(path)
+#     package_dir = info.data['project_dir']
+#     abs_path = p.join(package_dir, path)
 
-def _does_path_exist(path: Optional[str] = None,
-                     info: Optional[ValidationInfo] = None) -> bool:
-  if path:
-    path = p.abspath(path)
-    package_dir = info.data['project_dir']
-    abs_path = p.join(package_dir, path)
+#     if not p.exists(abs_path):
+#       raise ValueError(f'Path "{path}" does not exist')
+#   return path
 
-    if not p.exists(abs_path):
-      raise ValueError(f'Path "{path}" does not exist')
-  return path
+# class JxaProjectConfig(BaseModel):
+#   model_config: ConfigDict = ConfigDict(
+#       alias_generator=to_camel,
+#       str_strip_whitespace=True,
+#       extra='forbid',
+#       validate_default=True,
+#   )
+#   project_dir: str
+#   comp_mode: Literal['app', 'script', 'bundle'] = 'app'
+#   deps_comp_mode: Literal['script', 'bundle'] = 'script'
+#   deps_install_mode: Literal['app', 'user', 'system'] = 'app'
+#   version: Annotated[str, StringConstraints(pattern=SEM_VER)] = '1.0.0'
+#   app_name: str = 'out'
+#   main: str = 'index.js'
+#   app_icon: Optional[str] = None
+
+#   @field_validator('project_dir')
+#   @classmethod
+#   def project_dir_must_exist(cls, value: str) -> str:
+#     abs_path = p.abspath(value)
+#     if not p.exists(abs_path):
+#       raise ValueError(f'Path "{abs_path}" does not exist')
+#     if not p.isdir(abs_path):
+#       raise ValueError(f'Path "{abs_path}" is not a directory')
+#     return abs_path
+
+#   @field_validator('main', 'app_icon')
+#   @classmethod
+#   def path_must_exist(cls, value: Union[str, None],
+#                       info: ValidationInfo) -> str:
+#     if value:
+#       package_dir = info.data['project_dir']
+#       abs_path = p.join(package_dir, value)
+
+#       if not p.exists(abs_path):
+#         raise ValueError(f'Path "{abs_path}" does not exist')
+#       if not p.isfile(abs_path):
+#         raise ValueError(f'Path "{abs_path}" is not a file')
+#       return value
 
 
-class JxaProjectConfig(BaseModel):
-  model_config: ConfigDict = ConfigDict(
-      alias_generator=to_camel,
-      str_strip_whitespace=True,
-      extra='forbid',
-      validate_default=True,
-  )
+@dataclass
+class LoadedPropInfo:
+  value: any
+  origin: str
+  original_name: Optional[str] = None
+
+
+@dataclass
+class JxaProjectConfig:
   project_dir: str
   comp_mode: Literal['app', 'script', 'bundle'] = 'app'
   deps_comp_mode: Literal['script', 'bundle'] = 'script'
   deps_install_mode: Literal['app', 'user', 'system'] = 'app'
-  version: Annotated[str, StringConstraints(pattern=SEM_VER)] = '1.0.0'
+  version: str = '1.0.0'
   app_name: str = 'out'
   main: str = 'index.js'
   app_icon: Optional[str] = None
 
-  @field_validator('project_dir')
-  @classmethod
-  def project_dir_must_exist(cls, value: str) -> str:
-    abs_path = p.abspath(value)
-    if not p.exists(abs_path):
-      raise ValueError(f'Path "{abs_path}" does not exist')
-    if not p.isdir(abs_path):
-      raise ValueError(f'Path "{abs_path}" is not a directory')
-    return abs_path
 
-  @field_validator('main', 'app_icon')
-  @classmethod
-  def path_must_exist(cls, value: Union[str, None],
-                      info: ValidationInfo) -> str:
-    if value:
-      package_dir = info.data['project_dir']
-      abs_path = p.join(package_dir, value)
-
-      if not p.exists(abs_path):
-        raise ValueError(f'Path "{abs_path}" does not exist')
-      if not p.isfile(abs_path):
-        raise ValueError(f'Path "{abs_path}" is not a file')
-      return value
-
-
-try:
-  config = JxaProjectConfig(
-      projectDir='/Users/sebastian/Documents/src/js/DoubleClickConsoleEdit',
-      main='src/index.js',
-      appIcon='icon.icns')
-  print(config.model_dump_json(indent=2))
-except Exception as e:
-  print(e)
+# try:
+#   config = JxaProjectConfig(
+#       project_dir='/Users/sebastian/Documents/src/js/DoubleClickConsoleEdit',
+#       main='src/index.js',
+#       app_icon='icon.icns')
+#   print(config)
+# except Exception as e:
+#   print(e)
