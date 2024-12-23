fe8be9e24ac8797919b754d79219c5ad3b1454f1
seb0xff <contact.git@seb0xff.dev>
2024-12-23 13:29:18 +0100

    Remove some unnecessary code

--- jxa_builder/utils/click_importer.py
+++ jxa_builder/utils/click_importer.py
@@ -1,18 +1,6 @@
-import os
 from ..core.constants import IS_RICH
 
 if IS_RICH:
   import rich_click as click
 else:
   import click
-
-# def is_rich():
-#   return os.environ.get('JXA_BUILDER_CLASSIC_DISPLAY_MODE',
-#                         'false').lower() not in ('true', '1', 'yes')
-
-# def get_click():
-#   if is_rich():
-#     import rich_click as click
-#   else:
-#     import click
-#   return click
--- jxa_builder/utils/remove_empty_values.py
+++ jxa_builder/utils/remove_empty_values.py
@@ -12,40 +12,10 @@ def remove_empty_values(
   Args:
       d (Dict[str, any]): The dictionary to remove empty values from.
       exclude (Optional[Iterable], optional): Values to exclude from removal. Defaults to (0, False).
-  
-  Example:
-  ```python
-  x = remove_empty_values({
-      'a': 0,
-      'b': None,
-      'c': '',
-      'd': {},
-      'e': True,
-      'f': ({}, 0, None, ()),
-      'g': {
-          'a': 1,
-          'b': (),
-          'c': {
-              'a': [],
-              'b': False
-          }
-      }
-  })
-  print(x)
-  # {
-  #    'a': 0,
-  #    'e': True,
-  #    'f': ({}, 0, None, ()),
-  #    'g': {
-  #        'a': 1,
-  #        'c': {
-  #            'b': False
-  #        }
-  #    }
-  # }
-  ```
   """
+  if not isinstance(d, Dict):
+    raise ValueError(f'Expected a dictionary, got {type(d)}')
   return {
-      k: remove_empty_values(v, exclude=exclude) if isinstance(v, dict) else v
+      k: remove_empty_values(v, exclude=exclude) if isinstance(v, Dict) else v
       for k, v in d.items() if v or v == 0 or v in exclude
   }
